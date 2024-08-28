5ee7e77b7c2df7b97cae8c4a84b2463c74cd8b78
minhtranb <minhtranb.ezstudy@gmail.com>
2024-08-28 23:36:10 +0700

    feat(#32): Finish fixed all the login error

--- app/components/common/inputs/Input.tsx
+++ app/components/common/inputs/Input.tsx
@@ -5,27 +5,28 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string
 }
 
-const Input: React.FC<InputProps> = (
-  { label, name, type, placeholder, className, ...props },
-  ref
-) => {
-  return (
-    <label className="form form-control w-full pb-3">
-      {label && (
-        <div className="label">
-          <span className="label-text text-zinc-950 font-medium">{label}</span>
-        </div>
-      )}
-      <input
-        type={type}
-        name={name}
-        className={cn('input input-bordered', className)}
-        placeholder={placeholder}
-        // ref={ref}
-        {...props}
-      />
-    </label>
-  )
-}
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ label, name, type, placeholder, className, ...props }, ref) => {
+    return (
+      <label className="form form-control w-full pb-3">
+        {label && (
+          <div className="label">
+            <span className="label-text text-zinc-950 font-medium">
+              {label}
+            </span>
+          </div>
+        )}
+        <input
+          type={type}
+          name={name}
+          className={cn('input input-bordered', className)}
+          placeholder={placeholder}
+          ref={ref}
+          {...props}
+        />
+      </label>
+    )
+  }
+)
 
 export default Input
