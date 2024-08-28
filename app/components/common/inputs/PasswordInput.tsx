13bbe5a6a011c43c4528a73d3325c1703f326430
minhtranb <minhtranb.ezstudy@gmail.com>
2024-08-28 23:36:10 +0700

    feat(#32): Finish fixed all the login error

--- app/components/common/inputs/PasswordInput.tsx
+++ app/components/common/inputs/PasswordInput.tsx
@@ -9,48 +9,49 @@ export interface InputProps
   label?: string
 }
 
-const PasswordInput: React.FC<InputProps> = (
-  { name, className, label, ...props },
-  ref
-) => {
-  const [showPassword, setShowPassword] = useState<boolean>(false)
-  const browser = useDetectBrowser()
-  const togglePassword = (
-    e: React.MouseEvent<HTMLButtonElement | MouseEvent>
-  ) => {
-    e.preventDefault()
-    setShowPassword((prev) => !prev)
-  }
-  return (
-    <label className="form form-control w-full pb-3">
-      {label && (
-        <div className="label">
-          <span className="label-text text-zinc-950 font-medium">{label}</span>
-        </div>
-      )}
-      <div className="relative">
-        <input
-          name={name}
-          type={showPassword ? 'text' : 'password'}
-          placeholder="Enter your password"
-          className={cn(
-            'w-full input input-bordered data-[invalid]:ring-error',
-            className
-          )}
-          // ref={ref}
-          {...props}
-        />
-        {browser !== 'Edge' && (
-          <button
-            onClick={togglePassword}
-            className="absolute flex justify-around top-1/2  right-6 -translate-y-1/2 text-lg"
-          >
-            {showPassword ? <Eye /> : <EyeClosed />}
-          </button>
+const PasswordInput = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ name, className, label, ...props }, ref) => {
+    const [showPassword, setShowPassword] = useState<boolean>(false)
+    const browser = useDetectBrowser()
+    const togglePassword = (
+      e: React.MouseEvent<HTMLButtonElement | MouseEvent>
+    ) => {
+      e.preventDefault()
+      setShowPassword((prev) => !prev)
+    }
+    return (
+      <label className="form form-control w-full pb-3">
+        {label && (
+          <div className="label">
+            <span className="label-text text-zinc-950 font-medium">
+              {label}
+            </span>
+          </div>
         )}
-      </div>
-    </label>
-  )
-}
+        <div className="relative">
+          <input
+            name={name}
+            type={showPassword ? 'text' : 'password'}
+            placeholder="Enter your password"
+            className={cn(
+              'w-full input input-bordered data-[invalid]:ring-error',
+              className
+            )}
+            ref={ref}
+            {...props}
+          />
+          {browser !== 'Edge' && (
+            <button
+              onClick={togglePassword}
+              className="absolute flex justify-around top-1/2  right-6 -translate-y-1/2 text-lg"
+            >
+              {showPassword ? <Eye /> : <EyeClosed />}
+            </button>
+          )}
+        </div>
+      </label>
+    )
+  }
+)
 
 export default PasswordInput
--- app/components/login/LoginCard.tsx
+++ app/components/login/LoginCard.tsx
@@ -4,32 +4,106 @@ import Input from '../common/inputs/Input'
 import { Logo } from '../common/svgs'
 import PasswordInput from '../common/inputs/PasswordInput'
 import React from 'react'
+import { isClerkAPIResponseError } from '@clerk/nextjs/errors'
 import { useForm } from 'react-hook-form'
+import { useRouter } from 'next/navigation'
+import { useSignIn } from '@clerk/nextjs'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const formSchema = z.object({
+  email: z.string().email().min(1, 'Email is required'),
+  password: z.string().min(1, 'Password is required'),
+})
+
+type FormValues = z.infer<typeof formSchema>
 
 const LoginCard = () => {
-  const { setValue, register } = useForm()
+  const router = useRouter()
+  const { signIn, setActive, isLoaded } = useSignIn()
+  const {
+    register,
+    formState: { errors },
+    setError,
+    handleSubmit,
+  } = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+  })
+  const onSubmit = async (data: FormValues) => {
+    if (!isLoaded) {
+      console.log('Is loaded')
+      return null
+    }
+
+    try {
+      if (signIn) {
+        const response = await signIn.create({
+          identifier: data.email,
+          password: data.password,
+        })
+
+        if (response.status === 'complete') {
+          setActive({ session: response.createdSessionId })
+          router.push('/')
+        }
+      }
+    } catch (error) {
+      if (isClerkAPIResponseError(error)) {
+        if (error.errors) {
+          const errorItem = error.errors[0]
+          const field = errorItem.meta?.paramName
+          if (field === 'password') {
+            setError('password', { message: 'Password is incorrect' })
+          } else if (field === 'identifier') {
+            setError('email', { message: 'Email is not found' })
+          } else {
+            setError('root', { message: errorItem.longMessage })
+          }
+        }
+      }
+    }
+  }
 
   return (
     <div className="grid w-full flex-grow items-center justify-center">
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="w-full space-y-6 rounded-2xl bg-white px-4 py-10 shadow-md ring-1 ring-black/5 sm:w-96 sm:px-8">
           <header className="grid place-items-center">
             <Logo className="w-44 h-44 pb-4" />
           </header>
-          <div className="space-y-4">
+          <div className="">
             <Input
+              {...register('email')}
               label="Email"
               name="email"
               type="text"
               placeholder="Enter your email"
             />
-            {/*error*/}
+            {errors.email && (
+              <p className="text-error font-medium text-sm">
+                {errors.email.message}
+              </p>
+            )}
           </div>
-          <PasswordInput label="Password" />
-          {/*error*/}
+          <div>
+            <PasswordInput label="Password" {...register('password')} />
+            {errors.password && (
+              <p className="text-error font-bold text-sm">
+                {errors.password.message}
+              </p>
+            )}
+          </div>
           <a className="pt-5 text-sm" href="/forgot-password">
             Forgot Password?
           </a>
+          <button
+            type="submit"
+            className={`btn ${
+              !isLoaded ? 'btn-disabled' : 'btn-secondary'
+            } w-full`}
+          >
+            Login
+          </button>
         </div>
       </form>
     </div>
